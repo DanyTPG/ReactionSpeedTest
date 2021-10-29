2a56df141cc0283720a2a76d7422fa16f76c72ee
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-10-30 01:24:49 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+var start, end, timeTaken, c = 0;
+var average = 0;
+var date = new Date();
+var color = 'green'
+
+function makeShapeAppear() {
+
+    document.getElementById("shape").style.background = color;	
+}
+
+async function appearAfterDelay() {
+	document.getElementById("shape").style.background = "#a10000";
+    timer = Math.floor(Math.random() * 2000 + 1000);
+    await setTimeout(makeShapeAppear, timer);
+    start = new Date().getTime() + timer;
+    document.getElementById("time").innerHTML = timer / 1000 ;	
+}
+
+
+document.getElementById("shape").onclick = function() {
+	document.getElementById("shape").innerHTML = "";
+    end = new Date().getTime();
+	timeTaken = end - start;
+
+	if(timeTaken < 0){
+		
+		document.getElementById("timeTaken").innerHTML = "TOO SOON !!";
+		document.getElementById("shape").style.display = "none";
+		document.getElementById("soon").style.display = "flex";
+		
+		return; 
+		}else{
+			
+			average+= timeTaken;
+			c++;
+			
+			if(c == 5) finish();
+			
+			document.getElementById("shape").style.background = "#a10000";
+
+			document.getElementById("timeTaken").innerHTML = timeTaken + " ms";
+
+			appearAfterDelay();
+			}
+}
+
+function finish(){
+	c = 0;
+	document.getElementById("shape").style.display = "none";
+	document.getElementById("start").style.display = "flex";
+	document.getElementById("start").innerHTML = "your average time: " + average/5;
+	average = 0;
+}
+	
+document.getElementById("soon").onclick = function() {
+	document.getElementById("soon").style.display = "none";
+	document.getElementById("shape").style.display = "block";
+	appearAfterDelay();
+}
+
+
+document.getElementById("start").onclick = function() {
+	document.getElementById("start").style.display = "none";
+	document.getElementById("shape").style.display = "block";
+	appearAfterDelay();
+}
